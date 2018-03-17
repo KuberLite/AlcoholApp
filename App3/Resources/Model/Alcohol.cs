682776057b1dd48b381015c6fd70882961aa3310
KuberLite <31598215+KuberLite@users.noreply.github.com>
2018-03-18 01:05:23 +0300

    Simple commit

---
+++ App3/Resources/Model/Alcohol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App3.Resources.Model
+{
+    class Alcohol
+    {
+        public int ID { get; set; }
+
+        public string NameAlcohol { get; set; }
+
+        public double Volume { get; set; }
+
+        public string Discription { get; set; }
+    }
+}
